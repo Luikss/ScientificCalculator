3be2186dcb1e680f93c8736f91f0c7e283c619b7
Opilane TTHK <andreluige@hot.ee>
2023-03-16 13:54:20 +0200

    CalculatorPageViewModel

---
+++ ScientificCalculator/ViewModels/CalculatorPageViewModel.cs
@@ -0,0 +1,123 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace ViewModels
+{
+    [INotifyPropertyChanged]
+    internal partial class CalculatorPageViewModel
+    {
+        [ObservableProperty]
+        private string inputText = "";
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+        [RelayCommand]
+        private void Reset()
+        {
+            CalculatedResult = "0";
+            InputText = "";
+            isSciOpWaiting = false;
+        }
+
+        [RelayCommand]
+        private void Calculate()
+        {
+            if (InputText.Length == 0)
+            {
+                return;
+            }
+
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            try
+            {
+                var inputString = NormalizeInputString();
+            }
+            catch
+            {
+                CalculatedResult = "NaN";
+            }
+        }
+
+        private string NormalizeInputString()
+        {
+            Dictionary<string, string> _opMapper = new()
+            {
+                {"×", "*"},
+                {"÷", "/"},
+                {"SIN", "Sin"},
+                {"COS", "Cos"},
+                {"TAN", "Tan"},
+                {"ASIN", "Asin"},
+                {"ACOS", "Acos"},
+                {"ATAN", "Atan"},
+                {"LOG", "Log"},
+                {"EXP", "Exp"},
+                {"LOG10", "Log10"},
+                {"POW", "Pow"},
+                {"SQRT", "Sqrt"},
+                {"ABS", "Abs"}
+            };
+
+            var returnString = InputText;
+
+            foreach (var key in _opMapper.Keys)
+            {
+                returnString = returnString.Replace(key, _opMapper[key]);
+            }
+
+            return returnString;
+        }
+
+        [RelayCommand]
+        private void Backspace()
+        {
+            if (InputText.Length > 0)
+            {
+                InputText = InputText[..^1];
+            }
+        }
+
+        [RelayCommand]
+        private void NumberInput(string key)
+        {
+            InputText += key;
+        }
+
+        [RelayCommand]
+        private void MathOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+
+            InputText += $" {op} ";
+        }
+
+        [RelayCommand]
+        private void RegionOperator(string op)
+        {
+            if (op == ")")
+            {
+                isSciOpWaiting = false;
+            }
+
+            InputText += op;
+        }
+
+        [RelayCommand]
+        private void ScientificOperator(string op)
+        {
+            InputText += $"{op}(";
+            isSciOpWaiting = true;
+        }
+    }
+}
